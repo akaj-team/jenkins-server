a8fe4d342b461434e0c322e4d3c8a5830537ab21
at-hangtran <hang.tran@asiantech.vn>
2019-03-01 11:38:23 +0700

    update scripts

--- scripts/createRunner.py
+++ scripts/createRunner.py
@@ -9,7 +9,7 @@ import shutil
 
 browser = sys.argv[1]
 count = sys.argv[2]
-testRunnerFileName="ParallelTestRunnerGenerate"
+testRunnerFileName="Parallel"+browser.capitalize()+"TestRunnerGenerate"
 testRunnerGeneratePath="src/test/java/generate/"
 print ('Create: ', count+' '+browser+' threads')
 templateFilePath = 'src/test/java/ParallelTestRunner.java'
@@ -27,23 +27,31 @@ for i in range(1,int(count)+1):
         if 'ParallelTestRunner' in line:
          print(line.replace("ParallelTestRunner",testRunnerFileName+str(i)),end='')
         elif 'CucumberTestReport' in line:
-         print(line.replace('CucumberTestReport','CucumberTestReport'+str(i)),end='')
+         print(line.replace('CucumberTestReport',browser.capitalize()+'CucumberTestReport'+str(i)),end='')
         else:
          print(line,end='')
   file.close()
 
 suite = ET.Element("suite",name="Suite")
 parameter={
-	'name':browser+' Test',
+	'name':browser.capitalize()+' Test',
 	'parallel':'methods',
 	'thread-count':count
 }
 test = ET.SubElement(suite, "test",**parameter)
-ET.SubElement(test,"parameter",name="server",value="http://172.16.110.169:4444/wd/hub")
+ET.SubElement(test,"parameter",name="server",value="http://localhost:4444/wd/hub")
 ET.SubElement(test,"parameter",name="browserName",value=browser)
 classes = ET.SubElement(test, "classes")
 for i in range(1,int(count)+1):
     ET.SubElement(classes, "class", name='generate.'+testRunnerFileName+str(i))
 tree = ET.ElementTree(suite)
 tree.write(open('src/test/resources/GridParallelSuite.xml',"wb"))	
+
 os.system("mvn clean test")
+if not os.path.isdir("jsontarget"):
+   os.makedirs("jsontarget")
+source = os.listdir("target/cucumber-reports")
+destination = "jsontarget/"
+for files in source:
+    if files.endswith(".json"):
+        shutil.copy("target/cucumber-reports/"+files,destination)
